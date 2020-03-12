54cbddfe7676a939b71462e734d70262411ccb9b
amit kumar <kumaramitrou@gmail.com>
2020-03-12 17:52:01 +0530

    Using PropTypes, Lecture 109.

--- ReactLearningProcess/firstreactapp/src/components/Persons/Person/Person.js
+++ ReactLearningProcess/firstreactapp/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.module.css';
 import Auxiliary from  '../../../hoc/Auxiliary';
@@ -15,5 +16,12 @@ class Person extends Component {
         </Auxiliary>);
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
